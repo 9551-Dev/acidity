20c0f58b9f6da46b2a5b21d52f8bbd3e79d94ae0
9551Dev <oliverekcaha@gmail.com>
2023-02-22 18:06:45 +0100

    update

--- main.lua
+++ main.lua
@@ -1,40 +1,33 @@
 local acidity = require("acidity")
 
+love.cc.quantize(true)
+
+local map = acidity.noise_map_simple(1,20)
+
+function map.raw.output_processor(t)
+    return math.abs(t*20)
+end
+
 function love.load()
     love.graphics.setLineWidth(2)
 end
 
 function love.draw()
-    local map = acidity.noise_map_complex(1000001,50,1,2,0.5)
-    local w,h = love.graphics.getDimensions()
 
-    local min  =  math.huge
-    local max  = -math.huge
+    map.raw_config.frequency = math.ceil(os.clock())
+    map:rebuild()
+
+    local w,h = love.graphics.getDimensions()
 
     for x=1,w do
         for y=1,h do
             local c = map:get_point(x,y)
-            max = math.max(max,c)
-            min = math.min(min,c)
+
+            c = math.min(c,1)
+            c = math.max(c,-1)
+
             love.graphics.setColor(c,c,c)
             love.graphics.points(x,y)
         end
     end
-
-    --error(("%f:%f"):format(max,min))
-
-    --[[love.graphics.setColor(1,0,0)
-    for x,ls in pairs(map.vector_grid) do
-        x = x - 1
-        for y,vec in pairs(ls) do
-            y = y - 1
-            local nx = x * 32
-            local ny = y * 32
-
-            local enx = x*32 + vec.x*15
-            local eny = y*32 + vec.y*15
-
-            love.graphics.line(nx,ny,enx,eny)
-        end
-    end]]
 end
